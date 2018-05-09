0aa8af21cf1f73b7d1d153730ba029543124b0a6
arindam-bandyopadhyay <arindam.bandyopadhyay@oracle.com>
2018-05-09 16:13:09 +0530

    update

--- ci/gf-jenkins-infra/slave/generic/docker/files/gfbuildwrapper.sh
+++ ci/gf-jenkins-infra/slave/generic/docker/files/gfbuildwrapper.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
-mkdir -p /scratch/BUILD_AREA
+mkdir -p ${WORKSPACE}
 if [ $? -eq 0 ]; then
-    cd /scratch/BUILD_AREA
+   cd ${WORKSPACE}
 else
    echo "Unable to create directory ${WORKSPACE}"
 fi
-
-which java
 java -version | true
 shopt -s nocasematch
 if [[ ${REDIRECT_STD_ERR} == "true" ]]; then
- /bin/bash -ex ${EXECUTE_SCRIPT} dockertriggered 
+ /bin/bash -ex ${EXECUTE_SCRIPT} dockertriggered 2>> ${WORKSPACE}/debug.log
 else
  /bin/bash -ex ${EXECUTE_SCRIPT} dockertriggered
 fi
