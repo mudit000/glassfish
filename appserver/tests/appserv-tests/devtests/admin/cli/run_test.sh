dfe5070303e24f3e47b1c97a56519c67d586b8ef
Romain Grecourt <romain.grecourt@oracle.com>
2018-09-19 18:53:14 -0700

    fix tabs

--- appserver/tests/appserv-tests/devtests/admin/cli/run_test.sh
+++ appserver/tests/appserv-tests/devtests/admin/cli/run_test.sh
@@ -40,45 +40,45 @@
 
 
 test_run(){
-	export HUDSON=true
-	export ROOT=`pwd`
+  export HUDSON=true
+  export ROOT=`pwd`
 
-	if [ -x "/usr/bin/cygpath" ]
-	then
-	  ROOT=`cygpath -d ${ROOT}`
-	  echo "Windows ROOT: ${ROOT}"
-	  export CYGWIN=nontsec
-	fi
-	ant clean
-	time ant -Dnum_tests=45 ${TARGET} | tee ${TEST_RUN_LOG}
-	egrep 'FAILED *0' "${APS_HOME}/count.txt" >/dev/null
+  if [ -x "/usr/bin/cygpath" ]
+  then
+    ROOT=`cygpath -d ${ROOT}`
+    echo "Windows ROOT: ${ROOT}"
+    export CYGWIN=nontsec
+  fi
+  ant clean
+  time ant -Dnum_tests=45 ${TARGET} | tee ${TEST_RUN_LOG}
+  egrep 'FAILED *0' "${APS_HOME}/count.txt" >/dev/null
 }
 
 run_test_id(){
-	unzip_test_resources ${WORKSPACE}/bundles/glassfish.zip
-	cd `dirname ${0}`
-	test_init
-	get_test_target ${1}
-	test_run
-	check_successful_run
+  unzip_test_resources ${WORKSPACE}/bundles/glassfish.zip
+  cd `dirname ${0}`
+  test_init
+  get_test_target ${1}
+  test_run
+  check_successful_run
   generate_junit_report ${1}
   change_junit_report_class_names
 }
 
 
 get_test_target(){
-	case $1 in
-		admin_cli_all )
-			TARGET=all
-			export TARGET;;
-		* )
-			TARGET=$1
-			export TARGET;;
-	esac
+  case $1 in
+    admin_cli_all )
+      TARGET=all
+      export TARGET;;
+    * )
+      TARGET=$1
+      export TARGET;;
+  esac
 }
 
 list_test_ids(){
-	echo admin_cli_all admin-cli-group-1 admin-cli-group-2 admin-cli-group-3 admin-cli-group-4 admin-cli-group-5
+  echo admin_cli_all admin-cli-group-1 admin-cli-group-2 admin-cli-group-3 admin-cli-group-4 admin-cli-group-5
 }
 
 OPT=${1}
@@ -86,9 +86,9 @@ TEST_ID=${2}
 source `dirname ${0}`/../../../../common_test.sh
 
 case ${OPT} in
-	list_test_ids )
-		list_test_ids;;
-	run_test_id )
-		trap "copy_test_artifacts ${TEST_ID}" EXIT
-		run_test_id ${TEST_ID} ;;
+  list_test_ids )
+    list_test_ids;;
+  run_test_id )
+    trap "copy_test_artifacts ${TEST_ID}" EXIT
+    run_test_id ${TEST_ID} ;;
 esac
